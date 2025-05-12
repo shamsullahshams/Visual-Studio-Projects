ed909e83cf7accf57726fbbd5b66c0eab7e93a6b
shamszabul@gmail.com <shamszabul@gmail.com>
2025-05-12 20:39:41 +0100

    this is a commit

---
+++ Python/sample.py
@@ -0,0 +1,8 @@
+# Simple calculator: add two numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+result = num1 + num2
+
+print("The sum is:", result)
+
+print (" This is my first Python programm")
